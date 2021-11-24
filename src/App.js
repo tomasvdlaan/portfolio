07282b1547c27f2f8efbfc1bbbdbf5d0be48352c
Tomas van der Laan <tomasvdlaan@gmail.com>
2021-11-24 17:41:13 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import * as THREE from "three";
+class App extends Component {
+  componentDidMount() {
+    // === THREE.JS CODE START ===
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    const geometry = new THREE.InstancedBufferGeometry();
+
+    // positions
+    const positions = new THREE.BufferAttribute(new Float32Array(4 * 3), 3);
+    positions.setXYZ(0, -0.5, 0.5, 0.0);
+    positions.setXYZ(1, 0.5, 0.5, 0.0);
+    positions.setXYZ(2, -0.5, -0.5, 0.0);
+    positions.setXYZ(3, 0.5, -0.5, 0.0);
+    geometry.addAttribute("position", positions);
+
+    // uvs
+    const uvs = new THREE.BufferAttribute(new Float32Array(4 * 2), 2);
+    uvs.setXYZ(0, 0.0, 0.0);
+    uvs.setXYZ(1, 1.0, 0.0);
+    uvs.setXYZ(2, 0.0, 1.0);
+    uvs.setXYZ(3, 1.0, 1.0);
+    geometry.addAttribute("uv", uvs);
+
+    // index
+    geometry.setIndex(
+      new THREE.BufferAttribute(new Uint16Array([0, 2, 1, 2, 3, 1]), 1)
+    );
+
+    const indices = new Uint16Array(this.numPoints);
+    const offsets = new Float32Array(this.numPoints * 3);
+    const angles = new Float32Array(this.numPoints);
+
+    for (let i = 0; i < this.numPoints; i++) {
+      offsets[i * 3 + 0] = i % this.width;
+      offsets[i * 3 + 1] = Math.floor(i / this.width);
+
+      indices[i] = i;
+
+      angles[i] = Math.random() * Math.PI;
+    }
+
+    geometry.addAttribute(
+      "pindex",
+      new THREE.InstancedBufferAttribute(indices, 1, false)
+    );
+    geometry.addAttribute(
+      "offset",
+      new THREE.InstancedBufferAttribute(offsets, 3, false)
+    );
+    geometry.addAttribute(
+      "angle",
+      new THREE.InstancedBufferAttribute(angles, 1, false)
+    );
+
+    // var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    camera.position.z = 5;
+    var animate = function () {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render(scene, camera);
+    };
+    animate();
+    // === THREE.JS EXAMPLE CODE END ===
+  }
+  render() {
+    return <div />;
+  }
 }
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
 export default App;
